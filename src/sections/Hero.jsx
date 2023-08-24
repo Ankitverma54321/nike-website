3f750da52a7fa5bddd7e8e3b4050348105524f60
Ankitverma <av78751170@gmail.com>
2023-08-24 22:48:35 +0530

    modifying hero tab to select desired image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
 import { shoes, statistics } from '../constants'
@@ -6,6 +7,9 @@ import { bigShoe1 } from '../assets/images'
 import ShoeCard from '../components/ShoeCard'
 
 const Hero = () => {
+
+  const [bigShoeImg,setBigShoeImg]=useState(bigShoe1)
+
   return (
     <section
     id='home'
@@ -45,7 +49,7 @@ const Hero = () => {
       <div className='relative flex-1 flex justify-center items-center xl:min-h-screen
       max-xl:py-40 bg-primary bg-hero bg-cover bg-center' >
         <img 
-        src={bigShoe1}
+        src={bigShoeImg}
         alt='shoe collection'
         width={610}
         height={500}
@@ -53,7 +57,7 @@ const Hero = () => {
         />
 
         
-        <div>
+        <div className='flex sm:6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
           {shoes.map((shoe)=>(
                <div key={shoe}>
                 <ShoeCard
