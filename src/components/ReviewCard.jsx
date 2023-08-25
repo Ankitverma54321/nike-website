777460e5de961bbf2438fc155543d14c7cad3cb8
Ankitverma <av78751170@gmail.com>
2023-08-25 23:55:16 +0530

    the app has been created fully

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {star} from "../assets/icons";
 
 const ReviewCard = ({imgURL,customerName,rating,feedback}) => {
   return (
@@ -7,14 +8,26 @@ const ReviewCard = ({imgURL,customerName,rating,feedback}) => {
         src={imgURL}
         alt='customer'
         className='rounded-full
-        object-cover w-[120px] h-[120px]'
+        object-cover w-[120px] h-[120px] '
         />
-        <p>
+        <p className='mt-6 max-w-sm text-center info-text'>
             {feedback}
         </p>
-        <div>
-            
-        </div>
+        <div className='mt-3 flex justify-center items-center gap-2.5'>
+        <img
+          src={star}
+          width={24}
+          height={24}
+          alt='rating star'
+          className='object-contain m-0'
+        />
+        <p className='text-xl font-montserrat text-slate-gray'>({rating})</p>
+      </div>
+      <h3 className='mt-1 font-palanquin text-3xl text-center font-bold'>
+        {customerName}
+      </h3>
+        
+      
 
     </div>
   )
