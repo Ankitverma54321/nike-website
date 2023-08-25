0207b78a71072e7c849a4b816902a230080d3900
Ankitverma <av78751170@gmail.com>
2023-08-25 18:53:00 +0530

    feedback section in progress

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ReviewCard = ({imgURL,customerName,rating,feedback}) => {
+  return (
+    <div className='flex-justify-center items-center flex-col'>
+        <img 
+        src={imgURL}
+        alt='customer'
+        className='rounded-full
+        object-cover w-[120px] h-[120px]'
+        />
+        <p>
+            {feedback}
+        </p>
+        <div>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default ReviewCard
